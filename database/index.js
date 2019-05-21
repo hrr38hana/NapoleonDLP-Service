a3042eabab90c92bfa972e72e4ad96db704a8cea
NapoleonDLP <napoleon.dlp@gmail.com>
2019-05-21 02:00:18 -0700

    cleaned up ghost comments in file

--- database/index.js
+++ database/index.js
@@ -1,15 +1,11 @@
-//in here set up mongoose database
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-// var bodyParse = require('body-parser');
 mongoose.connect('mongodb://localhost/helmets', { useNewUrlParser: true });
 
-//this is the piece I was missing to be able to use the mongoose connect methods.
 var db = mongoose.connection;
 
 var dropDB = function() {
-  //access the DB as an array
   db.collections['helmets'].drop(function(err) {
     console.log('HELMETS DROPPED');
   });
@@ -25,7 +21,6 @@ var helmetsSchema = new Schema({
 var Helmet = mongoose.model('Helmet', helmetsSchema);
 
 var save = function(list) {
-  // var list = fakeItems.fakeItems;
   for (var i = 0; i < list.length; i++) {
     var newHelmet = new Helmet({
       id: list[i].id,
@@ -38,7 +33,15 @@ var save = function(list) {
   }
 };
 
+var showList = async () => {
+  //returning as a promise/ use async await
+  var helmets = await db.collections['helmets'].find();
+  console.log('Helmets showing from DB: ', helmets)
+  return helmets;
+};
+
 module.exports = {
   save: save,
-  dropDB: dropDB
+  dropDB: dropDB,
+  showList: showList
 };
