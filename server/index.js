b9b5997cbd961de6291f171cb3a6b43cf5a0ed57
NapoleonDLP <napoleon.dlp@gmail.com>
2019-05-21 02:11:00 -0700

    removed mongoClient and other ghost comments

--- server/index.js
+++ server/index.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3017;
-// const {} = require('../database/index');
-// var fakeItems = require('../database/mockData');
-var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient
-// app.use(bodyParser.urlencoded());
+const bodyParser = require('body-parser');
+const {showList} = require('../database/index');
+
 
 app.use(bodyParser.json());
-
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// console.log(bodyParser(dropDB()));
-
-app.get('/showData', (req, res) => {
+app.get('/showData', async (req, res) => {
   //not sure how to access mongDB data from server
   // db.find().pretty()
-  res.send();
+  // var helmet = showHelmet(24)
+  console.log(req.body)
+  // var list = await showList();
+  // console.log(bodyParser(list.body));
+  // console.log('This is from from the server**', list);
+
+  // res.send(bodyParser(list.body));
+  res.send(list);
 });
-
-// console.log(dropDB());
-
-// console.log('query: ', test(), dropDB());
 
 app.get('/more', (req, res) => res.send('this endpoint scrolls helmets over'));
 
@@ -34,24 +31,3 @@ app.get('/helmet/colors', (req, res) => res.send('this endpoint '));
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-//not sure how to access a DB query from the server
-// console.log(save.db.db.find());
-
-
-MongoClient.connect('mongodb://localhost:3017/helmets', function (err, client) {
-  if (err) {
-    console.log('There is an error connecting to MongoClient');
-  };
-
-  var db = helmets.db('helmets');
-  console.log(db)
-
-  db.collection('helmets').find().toArray(function (err, result) {
-    if (err) {
-      console.log('There is an error finding db collection helmets');
-    }
-
-    console.log(result);
-  })
-})
